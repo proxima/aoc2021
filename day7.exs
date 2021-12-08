a992398ec4ae44e0da1d540bdefd985b73894dc6
Christopher Lee <proxima@botchris.com>
2021-12-07 20:39:03 -0500

    part one day 7

--- day7.exs
+++ day7.exs
@@ -1,9 +1,9 @@
 #!/usr/bin/env elixir
 
-defmodule Day3 do
+defmodule Day7 do
   @moduledoc """
-    --- Day 3: Binary Diagnostic ---
-    https://adventofcode.com/2021/day/3
+    --- Day 7: The Treachery of Whales ---
+    https://adventofcode.com/2021/day/7
   """
 
   def main() do
@@ -19,16 +19,28 @@ defmodule Day3 do
 
     File.stream!("./priv/#{base}.txt")
     |> Stream.map(&String.trim_trailing/1)
+    |> Stream.flat_map(&String.split(&1, ","))
+    |> Stream.map(&String.to_integer/1)
   end
 
   def part_one() do
-    input_stream()
+    locations = 
+      input_stream()
+      |> Enum.to_list()
+
+    {min, max} = Enum.min_max(locations)
+
+    Enum.map(min..max, fn pivot ->
+      Enum.reduce(locations, 0, fn location, acc ->
+        acc + Kernel.abs(location - pivot)
+      end)
+    end)
+    |> Enum.min()
   end
 
   def part_two() do
     input_stream()
   end
-
 end
 
-Day3.main()
+Day7.main()
