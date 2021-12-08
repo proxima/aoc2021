23a2e5d5b4e49c48248db7cd5f199afcf87cb294
Christopher Lee <proxima@botchris.com>
2021-12-07 20:20:51 -0500

    day 6

--- day6.exs
+++ day6.exs
@@ -11,7 +11,7 @@ defmodule Day6 do
     IO.inspect(part_two(), label: "Part Two")
   end
 
-  defp input_stream() do
+  defp input_map() do
     base = __MODULE__
      |> to_string()
      |> String.downcase()
@@ -19,18 +19,40 @@ defmodule Day6 do
 
     File.stream!("./priv/#{base}.txt")
     |> Stream.map(&String.trim_trailing/1)
-    |> Stream.map(&String.split(&1, ","))
+    |> Stream.flat_map(&String.split(&1, ","))
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
   end
 
   def part_one() do
-    input_stream()
-    |> Enum.to_list()
+    input_map()
+    |> simulate(80)
+    |> Enum.reduce(0, fn {_age, count}, acc -> acc + count end) 
   end
 
   def part_two() do
-    input_stream()
+    input_map()
+    |> simulate(256)
+    |> Enum.reduce(0, fn {_age, count}, acc -> acc + count end) 
   end
 
+  def simulate(populace, 0), do: populace
+
+  def simulate(populace, times) do
+    populace = generation(populace)
+    simulate(populace, times - 1)
+  end
+
+  def generation(populace) do
+    Enum.reduce(populace, %{}, fn {age, count}, acc ->
+      if age == 0 do
+        Map.put(acc, 8, count)
+        |> Map.update(6, count, &(&1 + count))
+      else
+        Map.update(acc, age - 1, count, &(&1 + count))
+      end
+    end)
+  end
 end
 
 Day6.main()
